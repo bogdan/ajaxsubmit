ff7b59366abd072cded7210ce49fb8c93295e7ec
Bogdan Gusiev <agresso@gmail.com>
2011-12-03 17:23:33 +0200

    Cleanup

--- compile.js
+++ compile.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
 var files = ["form-errors.js.coffee", "ajax-submit.js.coffee"];
-var pending = files.length;
 var source = "";
 
 var CoffeeScript = eval(fs.readFileSync("vendor/coffee-script.js", 'utf-8'));
